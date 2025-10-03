83ad2c6ef5f905d74d2ac8604ef2561415ddd0f3
Alireza Janaki <162058075+VoidbornGames@users.noreply.github.com>
2025-10-03 15:12:10 +0330

    Add files via upload

---
+++ SamplePlugin.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Net;
+using UltimateServer.Plugins;
+using UltimateServer.Services;
+
+namespace TestPlugin
+{
+    public class TestPlugin : IPlugin
+    {
+        public string Name => "TestPlugin";
+        public string Version => "2.1.155";
+
+
+        public Task OnLoadAsync(IPluginContext context)
+        {
+            return Task.CompletedTask;
+        }
+
+        public Task OnUpdateAsync(IPluginContext context)
+        {
+            return Task.CompletedTask;
+        }
+
+        public Task OnUnloadAsync()
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
